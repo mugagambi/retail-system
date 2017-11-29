d5a984add9bd6abe3dddc77e4efe9fe8f549661e
Kimeli Nixon <kimelinxn96@gmail.com>
2017-11-30 00:20:19 +0300

    added income admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from accounts import models
+
+
+class IncomeAccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'amount', 'created_at')
+    date_hierarchy = 'created_at'
+
 
 # Register your models here.
+admin.site.register(models.IncomeAccount, IncomeAccountAdmin)
