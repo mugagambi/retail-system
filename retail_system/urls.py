91cc03ae0a6c32d7ca20b4ea643d20d9298cc4d8
Humphrey Mugambi <mugagambi@gmail.com>
2017-11-29 23:58:11 +0300

    customized admin

--- retail_system/urls.py
+++ retail_system/urls.py
@@ -15,7 +15,28 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+url(
+    r'^admin/password_reset/$',
+    auth_views.PasswordResetView.as_view(),
+    name='admin_password_reset',
+),
+url(
+    r'^admin/password_reset/done/$',
+    auth_views.PasswordResetDoneView.as_view(),
+    name='password_reset_done',
+),
+url(
+    r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
+    auth_views.PasswordResetConfirmView.as_view(),
+    name='password_reset_confirm',
+),
+url(
+    r'^reset/done/$',
+    auth_views.PasswordResetCompleteView.as_view(),
+    name='password_reset_complete',
+),
 ]
