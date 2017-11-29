8ecca0fb06b5186a7e659a1a6823c9c7049ffb99
Susan Kwamboka <susankwamboka81@gmail.com>
2017-11-30 00:59:38 +0300

    Added admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from inventory import models
 
-# Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'total_units', 'remaining_units', 'unit_price', 'description', 'account')
+    list_select_related = True
+
+
+# Register your models here.,
+admin.site.register(models.Item, ItemAdmin)
